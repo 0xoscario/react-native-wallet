decc9906a3de318fc90cb2731bd43ecf79c2d1a7
Oscar Buck <buckoscarengr@gmail.com>
2020-07-12 20:52:23 +0800

    fix absolute path issues

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 /**
  * @format
  */
+import { enableES5 } from 'immer';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from 'src/store';
+
+enableES5();
 
 const store = configureStore();
 
 const App = () => {
   return (
-    <>
+    <React.StrictMode>
       <Provider store={store}>
       </Provider>
-    </>
+    </React.StrictMode>
   );
 };
 
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.json',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx'
+        ],
+        root: ['./']
+      }
+    ]
+  ]
 };
