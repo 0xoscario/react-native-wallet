3b8e5b4a4b9558bbc36bf148afa9083da7de242d
Oscar Buck <buckoscarengr@gmail.com>
2020-07-12 12:50:22 +0800

    add more test

--- src/utils/etherscan.test.ts
+++ src/utils/etherscan.test.ts
@@ -4,6 +4,24 @@
 import { EthereumChainId } from 'src/utils/constants';
 import { getEtherscanLink } from 'src/utils/etherscan';
 
-test('Mainnet', () => {
-  expect(getEtherscanLink(EthereumChainId.MAINNET, '0x', 'transaction')).toBe('https://etherscan.io/tx/0x');
+describe('getEtherscanLink', () => {
+  test('Mainnet', () => {
+    expect(getEtherscanLink(EthereumChainId.MAINNET, '0x', 'transaction')).toBe('https://etherscan.io/tx/0x');
+  });
+
+  test('Ropsten', () => {
+    expect(getEtherscanLink(EthereumChainId.ROPSTEN, '0x', 'transaction')).toBe('https://ropsten.etherscan.io/tx/0x');
+  });
+
+  test('Rinkeby', () => {
+    expect(getEtherscanLink(EthereumChainId.RINKEBY, '0x', 'transaction')).toBe('https://rinkeby.etherscan.io/tx/0x');
+  });
+
+  test('Goerli', () => {
+    expect(getEtherscanLink(EthereumChainId.GOERLI, '0x', 'transaction')).toBe('https://goerli.etherscan.io/tx/0x');
+  });
+
+  test('Kovan', () => {
+    expect(getEtherscanLink(EthereumChainId.KOVAN, '0x', 'transaction')).toBe('https://kovan.etherscan.io/tx/0x');
+  });
 });
