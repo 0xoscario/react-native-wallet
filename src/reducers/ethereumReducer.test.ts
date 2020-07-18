1e42ec0a87466b5cb03821d353623d00807ed1a5
Oscar Buck <buckoscarengr@gmail.com>
2020-07-18 16:16:12 +0800

    complete queryGasStation test

--- src/reducers/ethereumReducer.test.ts
+++ src/reducers/ethereumReducer.test.ts
@@ -3,7 +3,7 @@
  */
 import { Action } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
-import { queryGasStation } from 'src/actions/ethereumActions';
+import { UPDATE_GAS_STATION, queryGasStation } from 'src/actions/ethereumActions';
 import { configureStore } from 'src/store';
 import { AppStorage } from 'src/utils/app-storage';
 import { EthereumGasStation } from 'src/utils/types';
@@ -18,10 +18,30 @@ describe('ethereum reducer', () => {
   });
 
   test('gas station', async () => {
+    expect(store.getState().ethereum.gasStation).toBeNull();
     const dispatch: ThunkDispatch<EthereumGasStation, null, Action<string>> = store.dispatch;
     const result = await dispatch(queryGasStation());
     expect(result).toBe(true);
     const gasStation = await AppStorage.getEthereumGasStation();
     expect(gasStation).toBeTruthy();
+    expect(gasStation!.retrievedTimestamp).toBeGreaterThan(0);
+    expect(gasStation).toEqual(store.getState().ethereum.gasStation);
+
+    // cached
+    const result2 = await dispatch(queryGasStation());
+    expect(result2).toBe(false);
+    expect(gasStation).toEqual(store.getState().ethereum.gasStation);
+
+    // expired
+    gasStation!.retrievedTimestamp -= 121 * 1000;
+    store.dispatch({
+      type: UPDATE_GAS_STATION,
+      payload: gasStation
+    });
+    const result3 = await dispatch(queryGasStation());
+    expect(result3).toBe(true);
+    const newGasStation = await AppStorage.getEthereumGasStation();
+    expect(gasStation).not.toEqual(newGasStation);
+    expect(newGasStation).toEqual(store.getState().ethereum.gasStation);
   });
 });
