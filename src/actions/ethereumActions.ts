c3d8a940291148f056277eb20fed2d0d0b3a0cfb
Oscar Buck <buckoscarengr@gmail.com>
2020-07-18 19:39:20 +0800

    add gasStation fields

--- src/actions/ethereumActions.ts
+++ src/actions/ethereumActions.ts
@@ -3,6 +3,7 @@
  */
 import { Action, Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
+import { BigNumber } from '@ethersproject/bignumber';
 import { AppStorage } from 'src/utils/app-storage';
 import axios from 'src/utils/axios';
 import { EthereumGasStation } from 'src/utils/types';
@@ -23,8 +24,23 @@ export function queryGasStation(): ThunkAction<
         return Promise.resolve(false);
       }
       const response = await axios.get('https://ethgasstation.info/json/ethgasAPI.json');
+
+      const {
+        safeLow, // Recommended safe(expected to be mined in < 30 minutes) gas price in x10 Gwei(divite by 10 to convert it to gwei)
+        average, // Recommended average(expected to be mined in < 5 minutes) gas price in x10 Gwei(divite by 10 to convert it to gwei)
+        fast, // Recommended fast(expected to be mined in < 2 minutes) gas price in x10 Gwei(divite by 10 to convert it to gwei)
+        fastest, // Recommended fastest(expected to be mined in < 30 seconds) gas price in x10 Gwei(divite by 10 to convert it to gwei)
+        block_time, // Average time(in seconds) to mine one single block
+        blockNum, // Latest block number
+      } = response.data;
       const gasStation: EthereumGasStation = {
         retrievedTimestamp: Date.now(),
+        safeLow: BigNumber.from(safeLow).div(10).toNumber(),
+        average: BigNumber.from(average).div(10).toNumber(),
+        fast: BigNumber.from(fast).div(10).toNumber(),
+        fastest: BigNumber.from(fastest).div(10).toNumber(),
+        blockTime: block_time,
+        blockNum: blockNum
       };
       AppStorage.setEthereumGasStation(gasStation);
 
