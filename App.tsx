f5bc2648447b30bdf59d3addcb89039954b0c51c
Oscar Buck <buckoscarengr@gmail.com>
2020-07-23 16:27:11 +0800

    add aes encryptor

--- App.tsx
+++ App.tsx
@@ -6,17 +6,29 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from 'src/store';
 import { Encryptor } from 'src/utils/encryptor';
-import { ethers } from 'ethers';
 
 enableES5();
 
 const store = configureStore();
 
 const App = () => {
-  (new Encryptor()).encrypt('', {});
-  const wallet = ethers.Wallet.createRandom();
-  console.log(wallet);
-  console.log(ethers.utils.base64.encode(ethers.utils.randomBytes(32)));
+  const test = async (): Promise<void> => {
+    const data = 'Hello, ZMWallet!';
+    const password1 = 'password1';
+    const password2 = 'password2';
+    const encryptor = new Encryptor();
+    const encryptedText1 = await encryptor.encrypt(password1, data);
+    const encryptedData1 = await encryptor.decrypt(password1, encryptedText1);
+    console.log(encryptedText1);
+    console.log(encryptedData1);
+  
+    const encryptedText2 = await encryptor.encrypt(password2, data);
+    const encryptedData2 = await encryptor.decrypt(password2, encryptedText2);
+    console.log(encryptedText2);
+    console.log(encryptedData2);
+  };
+  test();
+
   return (
     <React.StrictMode>
       <Provider store={store}>
--- src/utils/encryptor.ts
+++ src/utils/encryptor.ts
@@ -1,13 +1,42 @@
 /**
  * @format
  */
+import { ethers } from 'ethers';
 import { NativeModules } from 'react-native';
 
 const Aes = NativeModules.Aes;
 
 export class Encryptor {
-  encrypt = async (password: string, data: object): Promise<string> => {
-    console.log(await Aes.randomKey(16));
-    return '';
-  }
+  private generateSalt = (): string => {
+    return ethers.utils.base64.encode(ethers.utils.randomBytes(32));
+  };
+
+  private generateKey = async (password: string, salt: string): Promise<string> => {
+    return await Aes.pbkdf2(password, salt, 2048, 256);
+  };
+
+  private encryptWithKey = async (text: string, key: string): Promise<any> => {
+    const iv = await Aes.randomKey(16);
+    const cipher = await Aes.encrypt(text, key, iv);
+    return { cipher, iv };
+  };
+
+  private decryptWithKey = async (encryptedData: any, key: string): Promise<string> => {
+    return await Aes.decrypt(encryptedData.cipher, key, encryptedData.iv);
+  };
+
+  encrypt = async (password: string, data: any): Promise<string> => {
+    const salt = this.generateSalt();
+    const key = await this.generateKey(password, salt);
+		const result = await this.encryptWithKey(JSON.stringify(data), key);
+		result.salt = salt;
+		return JSON.stringify(result);
+  };
+
+  decrypt = async (password: string, encryptedText: string): Promise<any> => {
+    const encryptedData = JSON.parse(encryptedText);
+		const key = await this.generateKey(password, encryptedData.salt);
+    const text = await this.decryptWithKey(encryptedData, key);
+    return JSON.parse(text);
+  };
 }
