68719258d30a74b6251a662f80cfb210afde060f
Oscar Buck <buckoscarengr@gmail.com>
2020-07-25 06:46:01 +0800

    add more config for redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,18 +4,20 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import { applyMiddleware, createStore } from 'redux';
 import { persistReducer, persistStore } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from 'src/reducers';
-
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-};
 
 export function configureStore() {
   const middlewares = [thunkMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares)
   
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: AsyncStorage,
+    tateReconciler: autoMergeLevel2
+  };
   const persistedReducer = persistReducer(persistConfig, rootReducer);
   const store = createStore(persistedReducer, middlewareEnhancer);
   const persistor = persistStore(store);
