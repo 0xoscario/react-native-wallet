94a4519fd25911cac7e56d52ec126e2ba73ad35f
Oscar Buck <buckoscarengr@gmail.com>
2020-08-04 22:34:32 +0800

    add account name edit

--- src/layouts/wallet/home/extra/account-overview.component.tsx
+++ src/layouts/wallet/home/extra/account-overview.component.tsx
@@ -2,7 +2,12 @@
  * @format
  */
 import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import {
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 import { useDispatch } from 'react-redux';
 import Clipboard from '@react-native-community/clipboard';
 import {
@@ -18,10 +23,21 @@ import { useI18n } from 'src/i18n';
 import { spacingX, spacingY } from 'src/theme';
 
 export const AccountOverview = () => {
+  const [editAccount, setEditAccount] = React.useState({
+    editing: false,
+    name: ''
+  });
   const dispatch = useDispatch();
   const i18n = useI18n();
   const account = useAccount();
   const styles = useStyleSheet(themedStyles);
+
+  const handleChangeAccountName = () => {
+    setEditAccount({
+      editing: false,
+      name: ''
+    });
+  };
 
   const handleCopyAddress = () => {
     Clipboard.setString(account.address);
@@ -32,45 +48,69 @@ export const AccountOverview = () => {
   };
 
   return (
-    <View
-      style={styles.container}
+    <ScrollView
+      keyboardShouldPersistTaps={false}
     >
-      <TouchableOpacity
-        onPress={() => {}}
+      <View
+        style={styles.container}
       >
-        <Identicon
-          address={account.address}
-          size="large"
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.field}
-        onPress={() => {}}
-      >
-        <Text
-          category="h6"
+        <TouchableOpacity
+          onPress={() => {}}
         >
-          {account.name}
-        </Text>
-      </TouchableOpacity>
-      <Text
-        style={styles.field}
-        category="label"
-      >
-        $0
-      </Text>
-      <TouchableOpacity
-        style={[styles.field, styles.addressContainer]}
-        onPress={handleCopyAddress}
-      >
-        <EthereumAddress
-          style={styles.address}
-          appearance="hint"
+          <Identicon
+            address={account.address}
+            size="large"
+          />
+        </TouchableOpacity>
+        {editAccount.editing ? (
+          <TextInput
+            editable={editAccount.editing}
+            onChangeText={(value: string) => setEditAccount({
+              editing: true,
+              name: value
+            })}
+            onSubmitEditing={handleChangeAccountName}
+            onBlur={handleChangeAccountName}
+            value={editAccount.name}
+            selectTextOnFocus
+            returnKeyType="done"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        ) : (
+          <TouchableOpacity
+            style={styles.field}
+            onLongPress={() => setEditAccount({
+              editing: true,
+              name: account.name
+            })}
+          >
+            <Text
+              category="h6"
+            >
+              {account.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+        <Text
+          style={styles.field}
           category="label"
-          address={account.address}
-        />
-      </TouchableOpacity>
-    </View>
+        >
+          $0
+        </Text>
+        <TouchableOpacity
+          style={[styles.field, styles.addressContainer]}
+          onPress={handleCopyAddress}
+        >
+          <EthereumAddress
+            style={styles.address}
+            appearance="hint"
+            category="label"
+            address={account.address}
+          />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
